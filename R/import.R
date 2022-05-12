19ab30219e5bcea68ca21391d5c8474c12681dab
Zhangrenl <zhang_renliang@163.com>
2022-05-12 16:36:49 +0000

    !2 优化代码格式 * Merge branch 'master' of gitee.com:zhangrenl/quickhapr into dev * V0.99.6 * Merge branch 'dev' of gitee.com:zhangrenl/quickhapr into dev * foamt fixing * update README.md. * update README.md.

--- R/import.R
+++ R/import.R
@@ -1,49 +1,75 @@
 #' @name import_vcf
 #' @title import vcf from file
-#' @usage import_vcf(vcf_file = vcf_file, ...)
-#' @description read and import vcf file
-#' @importFrom vcfR read.vcfR
 #' @author Zhangrenl
+#' @usage import_vcf(vcf_file = vcf_file, ...)
 #' @description  import vcf file
+#' @examples
+#' \dontrun{
+#' library(quickHapR)
+#' data("quickHap_test")
+#' vcfR::write.vcf(vcf, file = "test.vcf.gz")
+#' vcf <- import_vcf(file = "test.vcf.gz")
+#' unlink("test.vcf.gz")
+#' }
+#' @importFrom vcfR read.vcfR
 #' @param vcf_file file path of vcf
 #' @param ... pass to vcfR::read.vcfR
 #' @usage import_vcf(vcf_file = vcf_file, ...)
 #' @export
+#' @return vcfR object
 import_vcf <- function(vcf_file = vcf_file, ...) {
-  vcf <- vcfR::read.vcfR(vcf_file, ...)
-  return(vcf)
+    vcf <- vcfR::read.vcfR(vcf_file, ...)
+    return(vcf)
 }
 
 
 #' @name import_pheno
 #' @title imports phenos from file
 #' @usage import_pheno(phenoFile, comment.char = "#", ...)
-#' @description 第一列为Accession
-#' 第二列起为表型，
-#' 第一行为表头（表型名称）：“表型名称（计量单位）.地点[年份]”
+#' @description first col should be Accessions
+#' phenos should loacted begin second col，
+#' phenoName should at the first row,
+#' If "." located in pheno name, the former part of phenoName will be set as y axis name
+#' and the latter part will be set as foot of the fig.
+#' @examples
+#' \dontrun{
+#' library(quickHapR)
+#' data("quickHap_test")
+#' write.table(pheno, file = "test.pheno.txt", quote = FALSE, sep = "\t")
+#' pheno <- import_pheno("test.pheno.txt")
+#' unlink("test.pheno.txt")
+#'}
 #' @importFrom utils read.delim
 #' @param phenoFile pheno file path, should be a table separated by tab
 #' @param comment.char comment.char, start with comment.char will be ignored
 #' @param ... parameters will pass to read.delim
 #' @export
+#' @return data.frame, Accession names were set as rownames and cols were
+#' named by pheno names
 import_pheno <- function(phenoFile, comment.char = "#", ...){
-  phenos <- utils::read.delim(phenoFile,
-                             check.names = F,
-                             row.names = 1,
-                             comment.char = comment.char, ...)
-  return(phenos)
+    phenos <- utils::read.delim(phenoFile,
+                                check.names = FALSE,
+                                row.names = 1,
+                                comment.char = comment.char, ...)
+    return(phenos)
 }
 
 
 #' @name import_gff
 #' @title  import_gff
-#' @usage import_gff(gffFile)
+#' @usage import_gff(gffFile, format = "GFF")
+#' @examples
+#' \dontrun{
+#'     library(quickHapR)
+#'     gff <- import_gff("your.gff", format = "GFF")
+#' }
 #' @importFrom rtracklayer import
-#' @param gffFile gffFile
+#' @param gffFile gff file path
+#' @param format defalt as GFF
 #' @export
-import_gff <- function(gffFile){
-  gff <- rtracklayer::import(gffFile)
-  return(gff)
+#' @return GRange object
+import_gff <- function(gffFile, format = "GFF"){
+    rtracklayer::import(gffFile, format = "GFF")
 }
 
 
@@ -59,7 +85,4 @@ import_gff <- function(gffFile){
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
 NULL
-
-
-
 
