c2de300a9188a348289a8aed048bf4ebbcdfb1f7
Zhangrenl <zhang_renliang@163.com>
2022-06-13 15:04:47 +0000

    update R/onload.R.

--- R/onload.R
+++ R/onload.R
@@ -2,7 +2,8 @@
     message("")
     message("lastupdate: 2022.6.6")
     message("Author: Zhang Ren Liang\n(Mail: zhang_renliang@163.com)")
-    message("")
+    message("quickHapr had renamed as geneHapR since 2022.6.10")
+    message("For new functions please use geneHapR instead.")
     options(stringsAsFactors = FALSE)
 }
 
